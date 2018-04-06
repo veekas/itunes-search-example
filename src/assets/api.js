adbe8ba6450824b614539d6df8c1ba46a304538e
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-06 09:49:17 -0400

    fix(api): change query to encodedQuery

--- src/assets/api.js
+++ src/assets/api.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 const searchiTunes = query => {
   const encodedQuery = encodeURIComponent(query);
-  const url = 'https://itunes.apple.com/search?term=' + query + '&media=music&entity=album&limit=10';
+  const url = 'https://itunes.apple.com/search?term=' + encodedQuery + '&media=music&entity=album&limit=10';
   return axios.get(url)
     .then(res => res.data.results)
     .catch(err => new Error(err.data));
