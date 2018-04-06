18e98c9cba06ec43b8a1506ba8b308cabb90faad
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-06 09:40:11 -0400

    feat(api): add iTunes api call

---
+++ src/assets/api.js
@@ -0,0 +1,11 @@
+var axios = require('axios');
+
+const searchiTunes = query => {
+  const encodedQuery = encodeURIComponent(query);
+  const url = 'https://itunes.apple.com/search?term=' + query + '&media=music&entity=album&limit=10';
+  return axios.get(url)
+    .then(res => res.data.results)
+    .catch(err => new Error(err.data));
+}
+
+module.exports = searchiTunes;
